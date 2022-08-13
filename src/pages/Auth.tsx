fd5173575ef8ae4004ea56f9024c4e14d40ab03a
baebumsoo <baebeomsu77@gmail.com>
2022-08-13 16:04:03 +0900

    Feat: 폼 타입 지정

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import AuthForm from "../components/Auth/AuthForm";
+import { FormType } from "../types/formType";
 
 const Auth = () => {
+  const [form, setForm] = useState<FormType>({
+    email: "",
+    password: "",
+  });
+  const [isSignUp, setIsSignUp] = useState<boolean>(false);
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return <AuthForm />;
 };
 
